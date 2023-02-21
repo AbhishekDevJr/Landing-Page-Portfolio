2338ee372d8b9cec330513787057cbf3c91ba843
Abhishek Choudhari <abhishek007coc@gmail.com>
2023-02-22 00:34:55 +0530

    NavBar component in Progress

--- landing-page-portfolio/src/App.js
+++ landing-page-portfolio/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-     Landing Page React App Development Environment Working
+     <NavBar />
     </div>
   );
 }
---
+++ landing-page-portfolio/src/components/NavBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function NavBar(){
+    return(
+        <div className = "nav">
+            <nav className = "navB">
+                <a href = "/">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="113" height="39" viewBox="0 0 113 39">
+                    <g id="Group_447" data-name="Group 447" transform="translate(-280 -656.995)">
+                    <text id="Peerblue_Exchange." data-name="Peerblue Exchange." transform="translate(280 692.995)" fill="#fff" fontSize="10" fontFamily="NotoSans-Regular, Noto Sans" letterSpacing="0.12em"><tspan x="0" y="0">Peerblue Exchange.</tspan></text>
+                    <g id="Group_1" data-name="Group 1" transform="translate(281.889 656.995)">
+                    <path id="Path_124" data-name="Path 124" d="M16.472-27.407s-.342-.1-.818-.2-1-.156-1-.156H2.1v4.873H13.4v.01h.791l.033-.005h.012c.031,0,.068,0,.109,0a1.669,1.669,0,0,1,1.667,1.667,1.669,1.669,0,0,1-1.667,1.667H6.973v-.01H2.1V-4.821H6.973v-9.84h7.861a7.452,7.452,0,0,0,1-.214,5.089,5.089,0,0,0,.9-.256,6.538,6.538,0,0,0,2.4-1.639,6.516,6.516,0,0,0,1.748-4.45A6.54,6.54,0,0,0,16.472-27.407ZM32.705-13.834H42a2.094,2.094,0,0,1,1.622,2.04,2.1,2.1,0,0,1-1.571,2.028H32.705Zm14.908-1.36a6.993,6.993,0,0,0-1.465-1.82c-.083-.073-.168-.144-.255-.214.035-.056.068-.113.1-.17v0l0,0,0-.008a.563.563,0,0,0,.03-.054,6.979,6.979,0,0,0,.884-3.4,6.984,6.984,0,0,0-.869-3.373c-.018-.035-.038-.068-.057-.1a7,7,0,0,0-6.047-3.5H27.832v4.958h4.873v-.007h7.782a2.105,2.105,0,0,1,1.549,2.021,2.086,2.086,0,0,1-1.678,2.053H27.832V-4.821H39.944a7.13,7.13,0,0,0,.79-.045,7.148,7.148,0,0,0,.791.045,6.98,6.98,0,0,0,6.1-3.588,6.978,6.978,0,0,0,.876-3.386,6.973,6.973,0,0,0-.884-3.4Zm7.862-12.569v4.873h20.5v-4.873Zm4.873,13.143H73.93v-4.873H60.349l-4.873-.03v14.7h20.5V-9.694H60.349Zm21.34,8.344L80.617-4.821h6.053l.273-.372,3.314-4.5-3.029-4.1Zm21.358,0L95.394-16.67l7.1-9.646,1.072-1.456H97.518l-.274.372L92.369-20.78,87.493-27.4l-.274-.372H81.168l1.072,1.455,7.1,9.647-.007.01,3.029,4.1,0,0,0,0L94.7-9.394l3.093,4.2.274.373h6.052Z" transform="translate(-2.1 27.898)" fill="#fff"/>
+                    <rect id="Rectangle_4" data-name="Rectangle 4" width="20.588" height="4.961" transform="translate(53.247 0.059)" fill="#f62da8"/>
+                    <path id="Path_125" data-name="Path 125" d="M-.637-.3h6.78L24.651,24.656H17.7Z" transform="translate(79.159 0.297)" fill="#f62da8"/>
+                    </g>
+                    </g>
+                    </svg>
+                </a>
+
+                <ul className = "nav-item2">
+                    <li><a href = "">How it works</a></li>
+                    <li><a href = "">About us</a></li>
+                    <li><a href = "">Partners</a></li>
+                    <li><a href = "">How it works</a></li>
+                    <li><a href = "">Support</a></li>
+                    <li id = "special-link"><a href = "">Join Community</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default NavBar;
