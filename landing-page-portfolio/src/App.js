e7a408eb91ecc6effe0136768a4f15a3b1ba01c8
Abhishek Choudhari <abhishek007coc@gmail.com>
2023-02-21 22:59:15 +0530

    Setup of Initial File Structure/BoilerPlate code cleanup

---
+++ landing-page-portfolio/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Landing Page React App Development Environment Working
+    </div>
+  );
+}
+
+export default App;
