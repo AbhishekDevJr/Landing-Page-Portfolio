508db84133d9364b78f714f22aa993913baa47f6
Abhishek Choudhari <abhishek007coc@gmail.com>
2023-02-22 16:37:16 +0530

    HeroSection component in progress

--- landing-page-portfolio/src/components/HeroSection.js
+++ landing-page-portfolio/src/components/HeroSection.js
@@ -5,13 +5,13 @@ function HeroSection(){
         <div className = "hero">
             <div className = "hero-content">
                 <div className = "hero-part1">
-                    <h1>Predict the Trend of Crypto. Earn as good as 2x in 5min.</h1>
+                    <h1>Predict the Trend of Crypto. Earn as<br /> good as 2x in 5min.</h1>
                     <p>Experience Trading Pair Prediction like Never Before</p>
                     <a href = "#">Get Started</a>
                 </div>
 
                 <div className = "hero-part2">
-                    <img src = "Mobile.png" alt = "Mobile Illustration" />
+                    <div className = "phone-edges"><img src = "iPhoneX.png" alt = "Mobile Illustration" /></div>
                 </div>
             </div>
         </div>
